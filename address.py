2f21959f9bd9bb074f5052c682d7d0809d2188bb
Brian Caulkins <briancaulkins@gmail.com>
2016-02-12 23:35:26 -0600

    Add first file of address project.  Laying ground work for idea.

---
+++ address.py
@@ -0,0 +1,7 @@
+first_name = raw_input("First Name: ")
+last_name = raw_input("Last Name: ")
+address = raw_input("Address: ")
+phone_num = raw_input("Mobile: ")
+email_address = raw_input("Email Address: ")
+
+print first_name, last_name, address, phone_num, email_address
