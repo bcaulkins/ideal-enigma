a90a7ff3a825b1bfef5e3b79377b92b16c6199d3
Brian Caulkins <briancaulkins@gmail.com>
2016-02-13 12:31:31 -0600

    First functional lines of code for my journal writing program.

--- address.py
+++ address.py
@@ -1,7 +1,17 @@
-first_name = raw_input("First Name: ")
-last_name = raw_input("Last Name: ")
-address = raw_input("Address: ")
-phone_num = raw_input("Mobile: ")
-email_address = raw_input("Email Address: ")
+import datetime
+f = open('journal.txt', 'a')
+now = datetime.datetime.now()
+entry_date=now.strftime("%m.%d.%Y %H:%M")
+journal_entry = raw_input("Start writing:")
+##last_name = raw_input("Last Name: ")
+##address = raw_input("Address: ")
+##phone_num = raw_input("Mobile: ")
+##email_address = raw_input("Email Address: ")
 
-print first_name+" "+last_name, address, phone_num, email_address
+##print f ##first_name+" "+last_name, address, phone_num, email_address
+
+
+f.write('\n'+entry_date+'\n'+'\n'+journal_entry+'\n')
+
+
+f.close()
