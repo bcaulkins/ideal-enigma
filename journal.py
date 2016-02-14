98814ac9b06059e5371d13a0f0d9dc8c34dded8a
Brian Caulkins <briancaulkins@gmail.com>
2016-02-13 19:54:03 -0600

    Modify to open file on each menu item.

--- journal.py
+++ journal.py
@@ -1,5 +1,4 @@
 import datetime
-f = open('journal.txt', 'a')
 now = datetime.datetime.now()
 entry_date=now.strftime("%m.%d.%Y %H:%M")
 entry_time=now.strftime("%H:%M")
@@ -10,17 +9,18 @@ while option:
 	1. New entry
 	2. Continue Current Entry
 	3. Exit""")
-	option =raw_input("What do you want to do?")
+	option =raw_input("What do you want to do? ")
 	if option == "1":
+		f = open('journal.txt', 'a')
 		print "Journal entry for "+entry_date+" :"
 		journal_entry = raw_input()
 		f.write('\n'+entry_date+'\n'+'\n'+journal_entry+'\n')
+		f.close()
 	elif option == "2":
+		f = open('journal.txt', 'a')
 		print "Continue entry @ "+entry_time
 		journal_entry = raw_input()
-		f.write('\n'+entry_time+'\n'+'\n'+journal_entry+'\n')		
+		f.write('\n'+entry_time+'\n'+'\n'+journal_entry+'\n')
+		f.close()		
 	elif option !="":
 		print '\n'"Not valid option"
-
-
-f.close()
